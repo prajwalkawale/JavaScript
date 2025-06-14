206571bacf8d49f366f9a77199642cd031696b7e
vicky <prajwalkawale01@hotmail.com>
2025-06-14 20:44:18 +0530

    Practised the Event Handling in JavaScript

--- Events/practice1.js
+++ Events/practice1.js
@@ -29,3 +29,28 @@ btn.addEventListener("click",()=>{
 //     console.log(currentMode);
 // })
 
+
+let dbclick = document.getElementById("newButton");
+
+// dbclick.addEventListener("dblclick",()=>{
+//     console.log("Double Clicked!!");
+// })
+
+
+// Event Object
+// It is a special object that has details about the event.
+// All event handlers have access to the Event object's properties and methods
+ 
+// event properties
+
+dbclick.onclick=(evt)=>{
+    console.log("Details about Event: ",evt);
+    console.log("Event Type: ",evt.type);
+    console.log("Event Targate: ",evt.target);
+    console.log("Event Position X-axis: ",evt.clientX);
+    console.log("Event Position Y-axis: ",evt.clientY);
+}
+
+dbclick.onclick=()=>{
+console.log("Override!!");   // The above block of code will not execute, this block of code will override the previous one
+}
