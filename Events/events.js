3372f1c6e7c7ae9592621c9ac9ac80ccecfc65a0
vicky <prajwalkawale01@hotmail.com>
2025-06-15 23:13:03 +0530

    Practised the Event Handling in JavaScript

--- Events/events.js
+++ Events/events.js
@@ -36,3 +36,50 @@ btn.addEventListener("click",()=>{
 
 
 btn.removeEventListener("click",removebtn);
+
+// Event propogation method
+
+let outerdiv = document.getElementById("outerdiv");
+let innerdiv = document.getElementById("innerdiv");
+
+outerdiv.addEventListener("click",(ent)=>{
+  console.log("Hello Outer Div!!")
+},false); // by default it is always false
+
+
+
+// innerdiv.addEventListener("click",(ent)=>{
+//   console.log("Hello Inner Div!!")  // This inner div will print its console. it follows bubbling up method
+//   // The execution will happen bottom to top
+//   // if we want to print only this specific event then we can use stop.propogation property.
+
+// },false) 
+
+
+
+
+
+// If you want to execute the code in a usual manner, top to bottom, then you can simply include the true value in the parameter
+
+// innerdiv.addEventListener("click",(ent)=>{
+//   console.log("Using capturing mode!")
+
+// },true) // it is known as capturing mode by adding true value 
+
+
+// Stop propogation method
+
+innerdiv.addEventListener("click",(ent)=>{
+  console.log("This is the only block of code that will print");
+  ent.stopPropagation();
+},false)
+
+
+// prevent default method
+
+let googleLink = document.querySelector("a");
+googleLink.addEventListener("click",(ent)=>{
+  console.log("This link will not take you on google search page!");
+  ent.preventDefault();
+})
+
